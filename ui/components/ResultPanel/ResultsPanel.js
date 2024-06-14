b98f5ba1fa937bd4b52cce13252fe0849b539603
igor <igor.danilov1824@gmail.com>
2024-06-14 10:08:39 +0300

    fix and refactor ResultsPanel component

--- ui/components/ResultPanel/ResultsPanel.js
+++ ui/components/ResultPanel/ResultsPanel.js
@@ -1,24 +1,36 @@
-import {getGoogle, getPlayer, subscribe} from "../../../core/state-manager.js";
+import {
+    _unSubscribe,
+    getGoogle,
+    getPlayer,
+    subscribe
+} from "../../../core/state-manager.js";
+import {EVENTS} from "../../../core/consts.js";
 
 
 export const ResultsPanelComponents =  () => {
     const element = document.createElement('div')
 
-    subscribe(() => {
-        render(element)
-    })
+    const observer = (e) => {
+       if(e.name === EVENTS.SCORE_CHANGED) {
+           render(element)
+       }
+    }
+
+    subscribe(observer)
 
     render(element)
 
-    return {element}
+    return {element, cleanup: () => {_unSubscribe(observer)}}
 }
-
 
 const render = async (element) => {
     element.innerHTML = ''
-    const google = await getGoogle()
-    const player1 = await getPlayer(1)
-    const player2 = await getPlayer(2)
+
+    const [google, player1, player2] = await Promise.all([
+        getGoogle(),
+        getPlayer(1),
+        getPlayer(2),
+    ]);
 
     element.append(`Google: ${JSON.stringify(google, null, 2)}, Player1: ${JSON.stringify(player1, null, 2)}, Player2: ${JSON.stringify(player2, null, 2)}`)
 }
